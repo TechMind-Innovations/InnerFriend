eb283397347d793c10315fe8f88fa59b9afce096
Matheus Henrique <mathheus.souza@outlook.com>
2024-06-26 13:23:51 -0300

    change: openai chatbot

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,3 +1,4 @@
+import base64
 def carrega(nome_do_arquivo):
     try:
         with open(nome_do_arquivo, "r") as arquivo:
@@ -5,3 +6,7 @@ def carrega(nome_do_arquivo):
             return dados
     except IOError as e:
         print(f"Erro no carregamento de arquivo: {e}")
+
+def encodar_imagem(caminho_imagem):
+    with open(caminho_imagem, "rb") as arquivo_imagem:
+        return base64.b64encode(arquivo_imagem.read()).decode('utf-8')
